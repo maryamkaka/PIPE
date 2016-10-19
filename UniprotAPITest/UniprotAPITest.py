f715d365d407e3c7ad31998a7eacdb6a42771065
Maryam K <mkaka139@gmail.com>
2016-10-19 11:39:03 -0400

    Output results to recievedData

---
+++ UniprotAPITest/UniprotAPITest.py
@@ -0,0 +1,26 @@
+import urllib.request
+import urllib.parse
+
+url = 'http://www.uniprot.org/uploadlists/'
+
+params = {
+	'from':'ACC', 
+	'to':'P_REFSEQ_AC', 
+	'format':'tab', 
+	'query':'P13368 P20806'
+}
+
+data = urllib.parse.urlencode(params)
+data = data.encode('ascii')
+
+request = urllib.request.Request(url, data)
+contact = "maryam.kaka@carleton.ca"
+request.add_header('User-Agent', 'Python %s' % contact)
+
+response = urllib.request.urlopen(request)
+protein = response.read()
+
+#write to file
+f = open('recievedData', 'wb')
+f.write(protein)
+f.close()
