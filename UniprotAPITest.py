c1ef8659d03b6266bd11ac911d40cbecd44c72e4
Maryam K <mkaka139@gmail.com>
2016-10-19 10:52:54 -0400

    Modified Uniprot Template to work without urllib2 package

--- UniprotAPITest.py
+++ UniprotAPITest.py
@@ -1,4 +1,5 @@
-import urllib, urllib2
+import urllib.request
+import urllib.parse
 
 url = 'http://www.uniprot.org/uploadlists/'
 
@@ -9,10 +10,13 @@ params = {
 	'query':'P13368 P20806'
 }
 
-data = urllib.urlencode(params)
-request = urllib2.request.Request(url, data)
+data = urllib.parse.urlencode(params)
+data = data.encode('ascii')
+
+request = urllib.request.Request(url, data)
 contact = "maryam.kaka@carleton.ca"
 request.add_header('User-Agent', 'Python %s' % contact)
-response = urllib2.urlopen(request)
-page = response.read(200000)
 
+response = urllib.request.urlopen(request)
+protein = response.read()
+print(protein)
