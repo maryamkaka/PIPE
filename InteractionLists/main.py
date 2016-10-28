eeafb2d931cd41a2adecb3b8f6695940c454a8ec
Maryam K <mkaka139@gmail.com>
2016-10-28 10:43:47 -0400

    Passed Test Case 2!

--- InteractionLists/main.py
+++ InteractionLists/main.py
@@ -1,13 +1,13 @@
 #load ProteinPfam into dictionary
 pfam = {}
-with open('ProteinFiles/TestCase/ProteinPfam.txt') as f:
+with open('ProteinFiles/TestCase/Case2/ProteinPfam.txt') as f:
     for line in f:
         line = line.strip('\n').split(' ')
         pfam[line[0]] = line[1:]
 
 #Load interactingDomainPairs info
 interactingDomains = {}
-with open('ProteinFiles/TestCase/interactingDomainPairs.txt') as f:
+with open('ProteinFiles/TestCase/Case2/interactingDomainPairs.txt') as f:
     for line in f:
         line = line.replace('\n', '').split(' ')
 
@@ -22,7 +22,7 @@ with open('ProteinFiles/TestCase/interactingDomainPairs.txt') as f:
             interactingDomains[line[1]] = [line[0]]
 
 #determine interacting domains for protein pair
-with open('ProteinFiles/TestCase/protein_pairs.txt') as f:
+with open('ProteinFiles/TestCase/Case2/protein_pairs.txt') as f:
     domainMatch = []
     one = open('InteractionLists/TestCase/one.txt', 'w')
     none = open('InteractionLists/TestCase/none.txt', 'w')
