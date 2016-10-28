5c589a0654452495db33c70381859cc6e660fcf6
Maryam K <mkaka139@gmail.com>
2016-10-28 10:33:54 -0400

    Small bug fixes in interactionsList script

--- InteractionLists/main.py
+++ InteractionLists/main.py
@@ -1,13 +1,13 @@
 #load ProteinPfam into dictionary
 pfam = {}
-with open('ProteinFiles/ProteinPfam.txt') as f:
+with open('ProteinFiles/TestCase/ProteinPfam.txt') as f:
     for line in f:
         line = line.strip('\n').split(' ')
-        pfam[line[0]] = line[1:-1]
+        pfam[line[0]] = line[1:]
 
 #Load interactingDomainPairs info
 interactingDomains = {}
-with open('ProteinFiles/interactingDomainPairs.txt') as f:
+with open('ProteinFiles/TestCase/interactingDomainPairs.txt') as f:
     for line in f:
         line = line.replace('\n', '').split(' ')
 
@@ -22,22 +22,23 @@ with open('ProteinFiles/interactingDomainPairs.txt') as f:
             interactingDomains[line[1]] = [line[0]]
 
 #determine interacting domains for protein pair
-with open('ProteinFiles/protein_pairs.txt') as f:
+with open('ProteinFiles/TestCase/protein_pairs.txt') as f:
     domainMatch = []
-    one = open('InteractionLists/one.txt', 'w')
-    none = open('InteractionLists/none.txt', 'w')
-    multi = open('InteractionLists/multi.txt', 'w')
+    one = open('InteractionLists/TestCase/one.txt', 'w')
+    none = open('InteractionLists/TestCase/none.txt', 'w')
+    multi = open('InteractionLists/TestCase/multi.txt', 'w')
 
     for line in f:
         domainMatch.append([])
 
-        line = line.strip('\n').replace(' ', '').split('\t')
+        line = line.strip('\n').split()
         proteinA = pfam[line[0]]
         proteinB = pfam[line[1]]
 
+        import pdb; pdb.set_trace()
         #check for interacting domains
         for dA in proteinA:
-            if(~(dA in interactingDomains)):    #break if current doamin has no interacting pairs
+            if((dA in interactingDomains) == False):    #break if current doamin has no interacting pairs
                 continue
 
             AInteractions = interactingDomains[dA]
