41b92efd0ab9288223e6ffc89b162e66bab997cc
Maryam <mkaka139@gmail.com>
2016-11-10 21:23:46 -0500

    Revert "Wrote function to retrieve protein length and lengths of respective domains"
    This reverts commit fc82e41b7e0a778c85dc49c96c0336d8cdf30a34.

--- InteractionLists/stats/stats.py
+++ InteractionLists/stats/stats.py
@@ -1,5 +1,4 @@
 import collections
-import untangle
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,21 +9,6 @@ verbose = 1
 def printDict(d):
     for i in d:
         print(i + ': ' + str(d[i]))
-
-def pfamConnect(protein):
-    url = 'http://pfam.xfam.org/protein/'
-
-    xml = untangle.parse(url + protein + '?output=xml').pfam.entry
-
-    proteinLength = int(xml.sequence['length'])
-    domainLength = {}
-
-    #extract data
-    for i in range(0, len(xml.matches.match)):
-        current = xml.matches.match
-        domainLength[current[i]['accession']] = int(current[i].location['end']) - int(current[i].location['start'])
-
-    return [proteinLength, domainLength]
 
 # Import data
 proteinA = []
