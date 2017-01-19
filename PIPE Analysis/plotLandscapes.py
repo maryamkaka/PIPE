c645ede9b16dc3971296541c552df0a8faef4495
Maryam K <mkaka139@gmail.com>
2017-01-19 12:49:56 -0500

    Plot 2D and 3D plots of landscapes

--- PIPE Analysis/plotLandscapes.py
+++ PIPE Analysis/plotLandscapes.py
@@ -6,31 +6,25 @@ import matplotlib.pyplot as plt
 import os
 import numpy
 
-landscapeLocation = '../pipe_results/landscapes/'
-outputLocation = '../ProteinFiles/landscapes/'
+landscapeLocation = 'pipe_results/landscapes/'
+outputLocation = 'ProteinFiles/landscapes/'
 landscapeList = os.listdir(landscapeLocation)
 
-#for landscape in landscapeList:
-    #plt.figure(0)
-    #plt.hist(list(proteins.values()), bins=50)
-    #plt.title(landscape)
-    #plt.xlabel()
-    #plt.ylabel()
-    #plt.grid(True)
-    #plt.savefig('.png', bbox_inches='tight')
+for landscape in landscapeList:
+    print(landscape)
 
-data = pd.read_csv(landscapeLocation + "A5YKK6-O75175.mat", sep=' ', header=None)
-data.as_matrix()
+    data = pd.read_csv(landscapeLocation + landscape, sep=' ', header=None)
+    data.as_matrix()
 
-# Set up grid and test data
-nx, ny = 735,2357
-x = range(nx)
-y = range(ny)
+    plt.figure()
+    plt.imshow(data, cmap='gray_r')
+    plt.savefig(outputLocation+ landscape[0:-4] + '.png', bbox_inches='tight')
+    plt.close()
 
-hf = plt.figure()
-ha = hf.add_subplot(111, projection='3d')
-
-X, Y = numpy.meshgrid(x, y)  # `plot_surface` expects `x` and `y` data to be 2D
-ha.plot_surface(X, Y, data, cmap=cm.coolwarm)
-
-plt.show()
+    ny, nx = data.shape
+    hf = plt.figure()
+    ha = hf.add_subplot(111, projection='3d')
+    X, Y = numpy.meshgrid(range(nx), range(ny))
+    ha.plot_surface(X, Y, data, cmap='cool')
+    plt.savefig(outputLocation + landscape[0:-4] + '-3D.png', bbox_inches='tight')
+    plt.close()
